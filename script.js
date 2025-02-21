12dab39317290babd63f5f3e423d48ecfd9b85d6
blien-vt <vtblien3005@gmail.com>
2025-02-21 14:30:14 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = user.value 
+  let p=pass.value
+  if (u=="blien" && p=="123")
+  { 
+  window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
